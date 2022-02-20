d0be5fae8ef3e02b48e8a3592bf6aad8a532bc14
reesericci <me@reeseric.ci>
2022-02-19 18:22:30 -0600

    meta tag updates

--- src/lib/Head.svelte
+++ src/lib/Head.svelte
@@ -1,5 +1,5 @@
 <script>
-    
+    import { base, assets } from '$app/paths';
 </script>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,18 +8,18 @@
 <title>shortie.sh | a very nerdy url shortener</title>
 <meta name="title" content="shortie.sh | a very nerdy url shortener">
 <meta name="description" content="A dead-simple url shortener that doesn't invade your privacy.">
-<link rel="icon" href="%svelte.assets%/shortie-icon.png" />
+<link rel="icon" href={`${assets}/shortie-icon.png`} />
 
 <!-- Open Graph / Facebook -->
 <meta property="og:type" content="website">
 <meta property="og:url" content="https://www.shortie.sh/">
 <meta property="og:title" content="shortie.sh | a very nerdy url shortener">
 <meta property="og:description" content="A dead-simple url shortener that doesn't invade your privacy.">
-<meta property="og:image" content="%svelte.assets%/shortie-banner.png">
+<meta property="og:image" content={`${assets}/shortie-banner.png`}>
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:url" content="https://www.shortie.sh/">
 <meta property="twitter:title" content="shortie.sh | a very nerdy url shortener">
 <meta property="twitter:description" content="A dead-simple url shortener that doesn't invade your privacy.">
-<meta property="twitter:image" content="%svelte.assets%/shortie-banner.png">
+<meta property="twitter:image" content={`${assets}/shortie-banner.png`}>
