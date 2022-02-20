72873100c49edb98c5acbfc2ed5199befd794311
reesericci <me@reeseric.ci>
2022-02-19 18:41:16 -0600

    i think?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -82,31 +82,6 @@
     }
 </script>
 
-<svelte:head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Primary Meta Tags -->
-    <title>shortie.sh | a very nerdy url shortener</title>
-    <meta name="title" content="shortie.sh | a very nerdy url shortener">
-    <meta name="description" content="A dead-simple url shortener that doesn't invade your privacy.">
-    <link rel="icon" href={`${assets}/shortie-icon.png`} />
-
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://www.shortie.sh/">
-    <meta property="og:title" content="shortie.sh | a very nerdy url shortener">
-    <meta property="og:description" content="A dead-simple url shortener that doesn't invade your privacy.">
-    <meta property="og:image" content={`${assets}/shortie-banner.png`}>
-
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://www.shortie.sh/">
-    <meta property="twitter:title" content="shortie.sh | a very nerdy url shortener">
-    <meta property="twitter:description" content="A dead-simple url shortener that doesn't invade your privacy.">
-    <meta property="twitter:image" content={`${assets}/shortie-banner.png`}>
-</svelte:head>
-
 <div on:click={() => modalOpen = !modalOpen} id="settings" class="absolute z-50 transition ease-out hover:animate-shortspin dark:fill-kindawhite fill-kindablack bottom-4 right-4">
     <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 0 24 24" width="32px">
     <path d="M0 0h24v24H0V0z" fill="none"></path>
